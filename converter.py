4ab7ec6cc27702fee231d85dd13c7cb188229243
bhuvanab777 <115543550+bhuvanab777@users.noreply.github.com>
2023-01-03 15:55:37 +0530

    Update converter.py

--- converter.py
+++ converter.py
@@ -9,10 +9,14 @@ with open('input.xml') as xml_file:
 
     json_conversion = json.dumps(parsed_data)
     
-    if os.getenv("ENABLE"): 
-        with open('output.json', 'w') as json_file:
+    with open('output.json', 'w') as json_file:
+        
+        if os.getenv("ENABLE"): 
+            
             json_file.write(json_conversion)
 
             json_file.close()
-    else:
-        print("Env variable is not set")
+        else:
+            json_file.write("Env variable is not set")
+            
+            json_file.close()
